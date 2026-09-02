2a3bc4f9c5c664badc966887648b7fa546541df2
profmauricioneto <mauricio.moreira@unichristus.edu.br>
2026-09-02 06:09:55 -0300

    add: aula sobre tailwindcss

--- curso-2026-2/aulas-react/primeiro-projeto-react/src/components/Exercicios2/Usuario.jsx
+++ curso-2026-2/aulas-react/primeiro-projeto-react/src/components/Exercicios2/Usuario.jsx
@@ -1,12 +1,13 @@
 const Usuario = ({ nome, idade, profissao }) => {
-    return(
-        <>
-            <h2>Olá {nome.toUpperCase()}</h2>
-            <h3>Idade: {idade}</h3>
-            <h3>Profissão: {profissao}</h3>
-            <hr />
-        </>
-    )
+    return (
+      <>
+        <div class="m-2 p-2 border-2 border-blue-400 rounded-md hover:bg-blue-400 text-white">
+          <h2 class="uppercase p-2">Olá {nome.toUpperCase()}</h2>
+          <h3>Idade: {idade}</h3>
+          <h3>Profissão: {profissao}</h3>
+        </div>
+      </>
+    );
 }
 
 export default Usuario;
--- curso-2026-2/aulas-react/primeiro-projeto-react/src/main.jsx
+++ curso-2026-2/aulas-react/primeiro-projeto-react/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import FirstComponent from './components/FirstComponent'
 // import Greeting from './components/Greetings'
-import {Saudacao, CalcularDataNascimento} from './components/Exercicio1'
+// import {Saudacao, CalcularDataNascimento} from './components/Exercicio1'
 import ListaUsuarios from './components/Exercicios2/ListaUsuarios'
 
 createRoot(document.getElementById('root')).render(
@@ -12,8 +12,8 @@ createRoot(document.getElementById('root')).render(
     {/* <h2>Hello World From React</h2>
     <FirstComponent />
     <Greeting name="Mauricio" age={36}/> */}
-    <Saudacao />
-    <CalcularDataNascimento age={36}/>
+    {/* <Saudacao /> */}
+    {/* <CalcularDataNascimento age={36}/> */}
     <ListaUsuarios />
   </StrictMode>,
 )
